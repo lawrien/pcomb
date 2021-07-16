1fafcb9bbe288fce4df74806ad95e28c72a790cb
Lawrie Nichols <lawrie@codesore.org>
2021-07-16 11:08:42 +0100

    updated tests

--- parsers/parsers_test.go
+++ parsers/parsers_test.go
@@ -38,13 +38,36 @@ func (suite *PcombSuite) TestMatch() {
 		{Seq(Char('b'), String("a")), "ab", false},
 	}
 
-	for i, test := range tests {
+	s := NewState()
 
-		s := NewState([]byte(test.Input))
-		res := s.Match(test.Parser)
+	for i, test := range tests {
+		res := s.Match([]byte(test.Input), test.Parser)
 
 		assert.Equal(suite.T(), test.Match, res.Ok, fmt.Sprintf("test [%d]. Expected %v, got %v", i, test.Match, res.Ok))
 	}
+}
+
+func (suite *PcombSuite) TestMapper() {
+
+	test := String("abc").Map(func(r Result) Result {
+		if r.Ok {
+			r.Bytes = []byte("Success")
+		} else {
+			r.Bytes = []byte("Failure")
+		}
+		return r
+	})
+
+	s := NewState()
+
+	r := s.Match([]byte("abc"), test)
+	assert.Equal(suite.T(), true, r.Ok, fmt.Sprintf("Expected %v, got %v", true, r.Ok))
+	assert.Equal(suite.T(), "Success", string(r.Bytes), fmt.Sprintf("Expected %v, got %v", "Success", string(r.Bytes)))
+
+	r = s.Match([]byte("cba"), test)
+	assert.Equal(suite.T(), false, r.Ok, fmt.Sprintf("Expected %v, got %v", false, r.Ok))
+	assert.Equal(suite.T(), "Failure", string(r.Bytes), fmt.Sprintf("Expected %v, got %v", "Failure", string(r.Bytes)))
+
 }
 
 func TestPcomb(t *testing.T) {
